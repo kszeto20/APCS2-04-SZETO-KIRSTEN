af28fd4ed8b4c1b7a30699e996ca4466b33c5d17
Kirsten Szeto <kszeto20@stuy.edu>
2021-02-08 02:44:32 -0500

    all edge cases for all methods tested and accounted for

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -5,18 +5,36 @@ public class Recursion {
     String test = "how much money you got?";
     System.out.println(reverse(test));
     System.out.println(reverse("a lot"));
+    */
+    ///*
     double myTest = (sqrt(Math.PI));
     double realAns = (Math.sqrt(Math.PI));
+
+    System.out.println("my test: " + myTest);
+    System.out.println("real ans: " + realAns);
+    System.out.println(((myTest - realAns) < (realAns * 0.00001)));
+
     myTest = sqrt(12345.0);
     realAns = (Math.sqrt(12345.0));
     System.out.println(myTest);
     System.out.println(realAns);
     System.out.println(((myTest - realAns) < (realAns * 0.00001)));
+
+    myTest = sqrt(0);
+    realAns = (Math.sqrt(0));
+    System.out.println(myTest);
+    System.out.println(realAns);
+    System.out.println(((myTest - realAns) < (realAns * 0.00001)));
+    //*/
+    /*
     System.out.println(countNoDoubleLetterWords(3));
     */
   }
 
   public static String reverse(String s){
+    if (s.length() == 0) {
+      return "";
+    }
     String test;
     if (s.length() == 2) {
       return s.substring(1) + s.substring(0, 1);
@@ -27,15 +45,18 @@ public class Recursion {
   }
 
   public static double sqrt(double n) {
-    return sqrt(n, 1.0, 0.00001);
+    return sqrt(n, 1.0);
   }
 
-  public static double sqrt(double n, double guess, double tolerance) {
-    if (Math.abs(guess * guess - n) <= tolerance) {
+  public static double sqrt(double n, double guess) {
+    if (n == 0) {
+      return 0;
+    }
+    if (Math.abs((guess * guess - n)/ n) <= 0.00001) {
         return guess;
     } else {
       System.out.println(guess);
-        return (sqrt(n, ((n / guess + guess) / 2), tolerance));
+        return (sqrt(n, ((n / guess + guess) / 2)));
     }
   }
 
