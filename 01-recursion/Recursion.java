97bcad93cf4814e9209fbc60fd621c464cd64612
Kirsten Szeto <kszeto20@stuy.edu>
2021-02-08 01:01:57 -0500

    my countDW prints something

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,9 +1,10 @@
 import java.lang.Math;
 public class Recursion {
   public static void main (String[] args) {
-    //String test = "how much money you got?";
-    //System.out.println(reverse(test));
-    //System.out.println(reverse("a lot"));
+    /*
+    String test = "how much money you got?";
+    System.out.println(reverse(test));
+    System.out.println(reverse("a lot"));
     double myTest = (sqrt(Math.PI));
     double realAns = (Math.sqrt(Math.PI));
     myTest = sqrt(12345.0);
@@ -11,7 +12,8 @@ public class Recursion {
     System.out.println(myTest);
     System.out.println(realAns);
     System.out.println(((myTest - realAns) < (realAns * 0.00001)));
-    //countNoDoubleLetterWords(2);
+    */
+    System.out.println(countNoDoubleLetterWords(2));
   }
 
   public static String reverse(String s){
@@ -27,7 +29,7 @@ public class Recursion {
   public static double sqrt(double n) {
     return sqrt(n, 1.0, 0.00001);
   }
-  
+
   public static double sqrt(double n, double guess, double tolerance) {
     if (Math.abs(guess * guess - n) <= tolerance) {
         return guess;
@@ -36,67 +38,28 @@ public class Recursion {
         return (sqrt(n, ((n / guess + guess) / 2), tolerance));
     }
   }
-}
 
-  /*
-
-  public static void countNoDoubleLetterWords (int length) {
-    System.out.println(countNoDoubleLetterWords(length, ""));
+  public static long countNoDoubleLetterWords(int length) {
+    return countNoDoubleLetterWords(length, "");
   }
-
-  public static long countNoDoubleLetterWords(int length,String word) {
-    if(length == 0) {
-      System.out.println(word);
-    }
-    else {
-      for (int i = 0; i < letters.length; i++) {
-        int lastChar = word.length() - 1;
-        if (word.length() > 0 && word.charAt(lastChar) == letters[i]) {
-        }
-        else {
-          printNoDoubleLetterWords(length - 1 , word + letters[i], letters);
-        }
-      }
-    }
-  }
-
-*/
-
-
-  /*
-    long combos = 0;
+  public static long countNoDoubleLetterWords(int length,String word){
+    long combos = 1;
     if(length == 0) {
       return combos;
     }
-    else if (length == 1) {
-      combos += (long)('z' - 'a' + 1);
-      return combos;
-    }
-    else {
-      for (int i = 'a'; i <= 'z'; i++) {
+    else{
+      for (int i = 0; i < 26; i++) {
         int lastChar = word.length() - 1;
-        if (word.length() > 0 && word.charAt(lastChar) == (char)(i)) {
-        }
+        if (word.length() > 0 && word.charAt(lastChar) == (char)('a')) {}
         else {
-          countNoDoubleLetterWords(length - 1);
+          combos++;
+          combos += countNoDoubleLetterWords(length - 1, word + (char)('a' + i));
         }
       }
     }
     return combos;
   }
-  */
-    /*
-    else {
-      for (int i = 0; i < letters.length; i++) {
-        int lastChar = word.length() - 1;
-        if (word.length() > 0 && word.charAt(lastChar) == letters[i]) {
-        }
-        else {
-          printNoDoubleLetterWords(length - 1 , word + letters[i], letters);
-        }
-      }
-    }
-    */
+}
   /*
   public static void printAllWords(int length) {
     printAllWords(length, "");
