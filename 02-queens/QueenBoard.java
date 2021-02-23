03e4b70ff3c275cbeaff86448224ddea2366723a
Kirsten Szeto <kszeto20@stuy.edu>
2021-02-23 14:14:48 -0500

    solve is putting down queens. Still cannot backtrack

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -15,49 +15,60 @@ public class QueenBoard {
 
     //check for toString
     //board[0][3] = -1;
-    board[1][0] = -1;
-    board[2][4] = -1;
+    //board[1][0] = -1;
+    //board[2][4] = -1;
     //board[3][1] = -1;
     //board[4][5] = -1;
+    //board[5][2] = -1;
   }
 
-//only use to String after solve() is completed
-  public String toString() {
-    String toRet = "";
-    //all rows except last
-    for (int i = 0; i < board.length - 1; i++) {
-      //all columns except last
-      for (int j = 0; j < board.length - 1; j++) {
-        if (board[i][j] == -1) {
-          toRet += "Q ";
-        }
-        else {
-          toRet += "_ ";
-        }
-      }
-      //last columns of all rows
-      if (board[i][board.length - 1] == -1) {
-        toRet += "Q\n";
-      }
-      else {
-        toRet += "_\n";
-      }
-    }
-    //last row
-    for (int j = 0; j < board.length; j++) {
-      if (board[board.length - 1][j] == -1) {
-        toRet += "Q ";
-      }
-      else {
-        toRet += "_ ";
-      }
-    }
-    return toRet;
+  public boolean solve() {
+    return (realSolve(0, 0, board.length));
   }
+
+  public boolean realSolve(int row, int col, int qNum) {
+    int qLeft = qNum;
+    if (qLeft == 0) {
+      return true;
+    }
+    else {
+      for (int i = 0; i < qNum - 1; i++) {
+        System.out.println("new row");
+        for (int j = 0; j < qNum - 1; j++) {
+          System.out.println("new column");
+          if (addQueen(i, j)) {
+            System.out.println("adding");
+            board[i][j] = -1;
+            qLeft--;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+/*
+  public static boolean nQueen(int row, int numOfQ) {
+    int numOfQLeft = numOfQ;
+      if (numOfQueens==0) {
+        return true;
+      }
+      for (int i = 1; i <= NumOfQLeft; i++) {
+        if(addQueen(row, j, board, N)) {
+          board[row][j] = 1;
+
+          if (nQueen(row+1, n-1, N, board))
+            return true;
+
+          board[row][j] = 0; //backtracking
+        }
+    }
+    return false;
+    }
+    */
 
   public boolean addQueen (int r, int c) {
     if (avail(r,c)) {
-      board[r][c] = -1;
       return true;
     }
     return false;
@@ -100,7 +111,7 @@ public class QueenBoard {
     }
     // Mission: given a point(x,y) I want to make sure there is no queen on the right diagonal
       // right diag clear?
-      for (int col = colPotQueen, row = rowPotQueen; col <= board.length && row >= 0; col++, row--) {
+      for (int col = colPotQueen, row = rowPotQueen; row >= 0 && col <= board.length - 1; col++, row--) {
         if (board[row][col] == -1) {
           System.out.println("upper right has queen");
           return false;
@@ -108,10 +119,44 @@ public class QueenBoard {
       }
     return true;
   }
+
+
+  //only use to String after solve() is completed
+    public String toString() {
+      String toRet = "";
+      //all rows except last
+      for (int i = 0; i < board.length - 1; i++) {
+        //all columns except last
+        for (int j = 0; j < board.length - 1; j++) {
+          if (board[i][j] == -1) {
+            toRet += "Q ";
+          }
+          else {
+            toRet += "_ ";
+          }
+        }
+        //last columns of all rows
+        if (board[i][board.length - 1] == -1) {
+          toRet += "Q\n";
+        }
+        else {
+          toRet += "_\n";
+        }
+      }
+      //last row
+      for (int j = 0; j < board.length; j++) {
+        if (board[board.length - 1][j] == -1) {
+          toRet += "Q ";
+        }
+        else {
+          toRet += "_ ";
+        }
+      }
+      return toRet;
+    }
 }
 /*
-Private methods (suggested):
-private void removeQueen(int r, int c) removing a queen should always work
+
 
 public boolean solve(){}
 public int countSolutions(){}
--- 02-queens/QueenTester.java
+++ 02-queens/QueenTester.java
@@ -4,12 +4,17 @@ public class QueenTester {
   public static void main (String[] args) {
     QueenBoard test = new QueenBoard(6);
     System.out.println(test.toString());
-    //System.out.println(avail(test.getBoard(), 0, 0));
-    System.out.println();
-    System.out.println(test.addQueen(3, 1));
+    test.solve();
     System.out.println(test.toString());
+    /*
+    System.out.println("-----------------");
+    System.out.println(test.addQueen(4, 2));
+    //System.out.println(test.toString());
     System.out.println("\n");
-    test.removeQueen(1, 0);
+    //test.removeQueen(2, 4);
     System.out.println(test.toString());
+    System.out.println(test.addQueen(4, 2));
+    System.out.println(test.toString());
+    */
   }
 }
