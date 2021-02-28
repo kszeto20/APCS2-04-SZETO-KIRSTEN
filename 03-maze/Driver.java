ed2f0b937e150c8445e3af01acb075db1fc48630
Kirsten Szeto <kszeto20@stuy.edu>
2021-02-28 00:53:33 -0500

    all files added + constructor and toString tested and run properly.

---
+++ 03-maze/Driver.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Driver{
+  public static void main(String[]args){
+    String filename = "data1.dat";
+    try{
+      Maze f;
+      f = new Maze(filename); //true animates the maze.
+      f.setAnimate(true); //comment this if you are using windows cmd/powershell
+      System.out.println(f);
+      //System.out.println(f.solve());
+      //System.out.println(f.solve()+" steps");
+
+      }
+      catch(FileNotFoundException e){
+      System.out.println("Invalid filename: "+filename);
+      }
+    }
+  }
