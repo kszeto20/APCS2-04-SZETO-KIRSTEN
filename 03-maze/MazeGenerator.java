19d0c4b54bf9db20c98f023041e7972d37a42e14
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-02 12:02:10 -0500

    makes straight corridors no variation yet

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,7 +1,90 @@
+import java.io.*;
+import java.util.*;
 public class MazeGenerator {
 
-  public static void generate(char[][]maze,rows,cols,startrow,startcol) {
-    System.out.println("wtf");
 
+  public static void generate(char[][] maze, int x, int y, int row, int column) {
+    // place
+    maze[row][column] = ' ';
+
+    //random gen
+    int[] poss = {0, 1, 2, 3};
+    for(int i = 0; i < poss.length; i++) {
+      Random r = new Random();
+      int j = Math.abs(r.nextInt(4));
+      int hold = poss[j];
+    }
+
+    //check + recursion
+    for(int i = 0; i < poss.length; i++) {
+      int random = poss[i];
+      if(random == 0) {
+        if(safe(maze, row, column + 1)) {
+          generate(maze, 0, 0, row, column + 1);
+        }
+      }
+      if(random == 1) {
+        if(safe(maze, row + 1, column)) {
+          generate(maze, 0, 0, row + 1, column);
+        }
+      }
+      if(random == 2) {
+        if(safe(maze, row, column - 1)) {
+          generate(maze, 0, 0, row, column - 1);
+        }
+      }
+      if (random == 3){
+        if(safe(maze, row - 1, column)) {
+          generate(maze, 0, 0, row - 1, column);
+        }
+      }
+    }
+  }
+
+  //safe to place?
+  private static boolean safe (char[][] maze, int row, int col) {
+    if (row <= 0 || row == maze.length - 1) {
+      return false;
+    }
+    if (col <= 0 || col == maze[0].length - 1) {
+      return false;
+    }
+
+    int surr = 0;
+
+    if (maze[row + 1][col] == ' ') {
+      surr++;
+    }
+    if (maze[row - 1][col] == ' ') {
+      surr++;
+    }
+    if (maze[row][col + 1] == ' ') {
+      surr++;
+    }
+    if (maze[row][col - 1] == ' ') {
+      surr++;
+    }
+
+    if (surr >= 2) {
+      return false;
+    }
+    else {
+      return true;
+    }
+
+  }
+
+  public static String toString(char[][] maze) {
+    String toRet = "";
+    for (int i = 0; i < maze.length - 1; i++) {
+      for (int j = 0; j < maze[0].length; j++) {
+        toRet += maze[i][j];
+      }
+      toRet += "\n";
+    }
+    for (int k = 0; k < maze[0].length; k++) {
+      toRet += maze[maze.length - 1][k];
+    }
+    return toRet;
   }
 }
