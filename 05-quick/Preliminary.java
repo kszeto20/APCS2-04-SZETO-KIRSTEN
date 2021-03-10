fc6c3061b3f2f66072b240bd406b52fff861e6fd
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-10 01:45:57 -0500

    moves p to the front

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -2,7 +2,33 @@ import java.io.*;
 import java.util.*;
 public class Preliminary {
 
-  public static int partition ( int [] data, int start, int end){
+  public static int partition (int [] data, int start, int end){
+    System.out.println(toString(data));
+
+    Random r = new Random();
+    int randIndex = r.nextInt(data.length);
+    System.out.println("RandomIndex: " + randIndex);
+    int pivot = data[randIndex];
+    System.out.println("Pivot: " + pivot);
+
+    int front = data[start];
+
+    data[start] = pivot;
+    data[randIndex] = front;
+
+    System.out.println(toString(data));
     return 1;
   }
+
+  public static String toString(int[] data) {
+    String toRet = "{";
+    for (int i = 0; i < data.length - 1; i++) {
+        toRet += data[i];
+        toRet += ", ";
+      }
+    toRet += data[data.length - 1];
+    toRet += "}";
+    return toRet;
+  }
+
 }
