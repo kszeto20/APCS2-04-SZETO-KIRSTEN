2623788e35e58cda97ea1441c3d687d91d429857
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-13 21:28:09 -0500

    works sometimes, at least it compiles

--- 05-quick/PrelimTester.java
+++ 05-quick/PrelimTester.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class PrelimTester {
 
   public static void main(String[] args) {
-    int[] test = {0, 999, 999, 1, 999, 100, 30, 8, 10, 999, 999, 999};
-    System.out.println(Preliminary.partition(test, 0, test.length));
+    int[] test = {999, 999, 999, 3, 4, 18, 25, 100, 330, 999, 999, 999};
+    //System.out.println(Preliminary.partition(test, 0, test.length));
+    System.out.println(Preliminary.quickselect(test, 0));
   }
 }
 
--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -3,30 +3,29 @@ import java.util.*;
 public class Preliminary {
 
   public static int partition (int [] data, int start, int end){
-    System.out.println(toString(data));
+//    System.out.println(toString(data));
 
     Random r = new Random();
-    int index = 1;
-    //r.nextInt(end - start) + start;
-    System.out.println("RandomIndex: " + index);
+    int index = r.nextInt(end - start) + start;
+//    System.out.println("RandomIndex: " + index);
     int pivot = data[index];
-    System.out.println("PivotValue: " + pivot);
+//    System.out.println("PivotValue: " + pivot);
     int front = data[start];
     data[start] = pivot;
     data[index] = front;
-    System.out.println("Initial move of P to the front: " + toString(data));
+//    System.out.println("Initial move of P to the front: " + toString(data));
     int bigInd = start + 1;
 
     for (int toScan = start + 1; toScan < end; toScan++) {
       if (data[toScan] < pivot) {
-        System.out.println("BEFORE: " + toString(data));
+//        System.out.println("BEFORE: " + toString(data));
         int storage = data[bigInd];
         data[bigInd] = data[toScan];
         data[toScan] = storage;
-        System.out.println("BigInd Before: " + bigInd);
+//        System.out.println("BigInd Before: " + bigInd);
         bigInd++;
-        System.out.println("BigInd After: " + bigInd);
-        System.out.println("Actual Smaller Case: " + toString(data) + "\n -------------");
+//        System.out.println("BigInd After: " + bigInd);
+//        System.out.println("Actual Smaller Case: " + toString(data) + "\n -------------");
       }
       else if (data[toScan] == pivot) {
         boolean bigSmallSplit = true;
@@ -34,36 +33,55 @@ public class Preliminary {
         int bool = b.nextInt(2);
         if (bool == 0) {
           bigSmallSplit = false;
-          System.out.println("System chose smaller!");
+//          System.out.println("System chose smaller!");
           int toStore = data[bigInd];
           data[bigInd] = data[toScan];
           data[toScan] = toStore;
-          System.out.println("BigInd Before: " + bigInd);
+//          System.out.println("BigInd Before: " + bigInd);
           bigInd++;
-          System.out.println("BigInd After: " + bigInd);
-          System.out.println("\n -------------");
+//          System.out.println("BigInd After: " + bigInd);
+//          System.out.println("\n -------------");
         }
         else {
-          System.out.println("System chose bigger!");
-          System.out.println("\n -------------");
+//          System.out.println("System chose bigger!");
+//          System.out.println("\n -------------");
         }
       }
       else {
-        System.out.println("DEBUG: bigInd --> " + bigInd + " | val --> " + data[bigInd]);
-        System.out.println("curToScan Val : "  + toScan);
-        System.out.println("\n -------------");
+//        System.out.println("DEBUG: bigInd --> " + bigInd + " | val --> " + data[bigInd]);
+//        System.out.println("curToScan Val : "  + toScan);
+//        System.out.println("\n -------------");
       }
-      System.out.println("AFTER: " + toString(data));
+//      System.out.println("AFTER: " + toString(data));
     }
 
     int temp = data[bigInd - 1];
     data[bigInd - 1] = data[start];
     data[start] = temp;
-    System.out.println(toString(data));
-    System.out.println("BigInd: " + bigInd);
-    System.out.println("PivotValue: " + data[bigInd - 1]);
-    System.out.println("Kirsten's Pivot Index: " + (bigInd - 1));
+//    System.out.println(toString(data));
+//    System.out.println("BigInd: " + bigInd);
+//    System.out.println("PivotValue: " + data[bigInd - 1]);
+//    System.out.println("Kirsten's Pivot Index: " + (bigInd - 1));
     return (bigInd - 1);
+  }
+
+  public static int quickselect (int[] data, int k) {
+    return quickSelect(data, k, 0, data.length - 1);
+  }
+
+  private static int quickSelect(int[] data, int k, int start, int end) {
+    if (start == end) {
+      return data[k];
+    }
+    int resultInd = partition(data, start, end);
+    if (resultInd < k) {
+      System.out.println(toString(data));
+      return quickSelect(data, k, resultInd + 1, data.length - 1);
+    }
+    else {
+      System.out.println(toString(data));
+      return quickSelect(data, k, 0, resultInd - 1);
+    }
   }
 
   public static String toString(int[] data) {
