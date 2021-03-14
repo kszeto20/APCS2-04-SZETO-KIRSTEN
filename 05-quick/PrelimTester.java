f62838c1e4abb3295e22446d64b967fe49ce4ced
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-14 01:54:55 -0500

    QUICKSELECT DONE

--- 05-quick/PrelimTester.java
+++ 05-quick/PrelimTester.java
@@ -3,10 +3,48 @@ import java.util.*;
 public class PrelimTester {
 
   public static void main(String[] args) {
-    int[] test = {999, 999, 999, 3, 4, 18, 25, 100, 330, 999, 999, 999};
+    int[] test = new int[10000000];
+    Random r = new Random();
+    for (int i = 0; i < 10000000; i++) {
+      test[i] = r.nextInt(100);
+    }
     //System.out.println(Preliminary.partition(test, 0, test.length));
-    System.out.println(Preliminary.quickselect(test, 10));
+    Arrays.sort(test);
+    System.out.println("--------------------");
+    int toRet = test[1000000];
+    System.out.println("DIRECT: " + toRet);
+    System.out.println(Preliminary.quickselect(test, 1000000));
   }
 }
 
-//0, 1, 8, 10, 30, 100
+//5, 12, 12, 22, 45, 192, 345, 7348, 23468
+
+/*
+
+import java.io.*;
+import java.util.*;
+public class PrelimTester {
+
+  public static void main(String[] args) {
+    int[] test = new int[10000000];
+
+    ArrayList<Integer> toShuffle = new ArrayList<Integer>(test.length);
+    for (int i = 0; i < test.length; i++) {
+      Random r = new Random();
+      toShuffle.add(r.nextInt(100));
+    }
+
+    Collections.shuffle(toShuffle);
+
+		for (int i = 0; i < test.length; i++) {
+      test[i] = toShuffle.get(i);
+    }//converts arraylist to array
+
+    System.out.println("-----------------------------");
+    //System.out.println(Preliminary.partition(test, 0, test.length));
+    System.out.println(Preliminary.quickselect(test, 3000000));
+  }
+}
+
+//5, 12, 12, 22, 45, 192, 345, 7348, 23468
+*/
--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -6,25 +6,24 @@ public class Preliminary {
 //    System.out.println(toString(data));
     Random r = new Random();
     int index = r.nextInt(end - start + 1) + start;
-    System.out.println("RandomIndex: " + index);
+//    System.out.println("RandomIndex: " + index);
     int pivot = data[index];
-    System.out.println("PivotValue: " + pivot);
-    for (int i = index; i > start; i--) {
-      data[i] = data[i - 1];
-    }
+//    System.out.println("PivotValue: " + pivot);
+    int front = data[start];
     data[start] = pivot;
-    System.out.println("Initial move of P to the front: " + toString(data));
+    data[index] = front;
+//    System.out.println("Initial move of P to the front: " + toString(data));
     int bigInd = start + 1;
 
     for (int toScan = start + 1; toScan <= end; toScan++) {
       if (data[toScan] < pivot) {
-        System.out.println("BEFORE: " + toString(data));
+//        System.out.println("BEFORE: " + toString(data));
         int storage = data[bigInd];
         data[bigInd] = data[toScan];
         data[toScan] = storage;
-        System.out.println("BigInd Before: " + bigInd);
+//        System.out.println("BigInd Before: " + bigInd);
         bigInd++;
-        System.out.println("BigInd After: " + bigInd);
+//        System.out.println("BigInd After: " + bigInd);
 //        System.out.println("Actual Smaller Case: " + toString(data) + "\n -------------");
       }
       else if (data[toScan] == pivot) {
@@ -33,25 +32,24 @@ public class Preliminary {
         int bool = b.nextInt(2);
         if (bool == 0) {
           bigSmallSplit = false;
-          System.out.println("System chose smaller!");
+//          System.out.println("System chose smaller!");
           int toStore = data[bigInd];
           data[bigInd] = data[toScan];
           data[toScan] = toStore;
-          System.out.println("BigInd Before: " + bigInd);
+//          System.out.println("BigInd Before: " + bigInd);
           bigInd++;
-          System.out.println("BigInd After: " + bigInd);
+//          System.out.println("BigInd After: " + bigInd);
 //          System.out.println("\n -------------");
         }
-        System.out.println("System chose larger!");
+//        System.out.println("System chose larger!");
       }
-      System.out.println("AFTER: " + toString(data));
+//      System.out.println("AFTER: " + toString(data));
     }
-    for (int i = 1; i < bigInd; i++) {
-      data[i - 1] = data[i];
-    }
+    int temp = data[bigInd - 1];
+    data[bigInd - 1] = data[start];
+    data[start] = temp;
 
-    data[bigInd - 1] = pivot;
-    System.out.println("DEBUG: Moved back into pivot spot" + toString(data));
+//    System.out.println("DEBUG: Moved back into pivot spot" + toString(data));
 //    System.out.println("BigInd: " + bigInd);
 //    System.out.println("PivotValue: " + data[bigInd - 1]);
 //    System.out.println("Kirsten's Pivot Index: " + (bigInd - 1));
@@ -63,19 +61,19 @@ public class Preliminary {
   }
 
   private static int quickSelect(int[] data, int k, int start, int end) {
-    System.out.println("Start --> " + start + " end --> " + end);
+//    System.out.println("Start --> " + start + " end --> " + end);
     int resultInd = partition(data, start, end);
     if (resultInd == k) {
       return data[k];
     }
     if (resultInd < k) {
-      System.out.println("After Initial Partition " + toString(data));
-      System.out.println("Returned Index: " + resultInd);
+//      System.out.println("After Initial Partition " + toString(data));
+//      System.out.println("Returned Index: " + resultInd);
       return quickSelect(data, k, resultInd, end);
     }
     else {
-      System.out.println("After Initial Partition " + toString(data));
-      System.out.println("Returned Index: " + resultInd);
+//      System.out.println("After Initial Partition " + toString(data));
+//      System.out.println("Returned Index: " + resultInd);
       return quickSelect(data, k, start, resultInd);
     }
   }
