17bfe70f068c9778540e8cdc70769bd75e212fea
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-14 00:32:29 -0500

    quick select works for all the cases I thought of

--- 05-quick/PrelimTester.java
+++ 05-quick/PrelimTester.java
@@ -5,7 +5,7 @@ public class PrelimTester {
   public static void main(String[] args) {
     int[] test = {999, 999, 999, 3, 4, 18, 25, 100, 330, 999, 999, 999};
     //System.out.println(Preliminary.partition(test, 0, test.length));
-    System.out.println(Preliminary.quickselect(test, 0));
+    System.out.println(Preliminary.quickselect(test, 10));
   }
 }
 
--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -9,21 +9,22 @@ public class Preliminary {
     System.out.println("RandomIndex: " + index);
     int pivot = data[index];
     System.out.println("PivotValue: " + pivot);
-    int front = data[start];
+    for (int i = index; i > start; i--) {
+      data[i] = data[i - 1];
+    }
     data[start] = pivot;
-    data[index] = front;
     System.out.println("Initial move of P to the front: " + toString(data));
     int bigInd = start + 1;
 
-    for (int toScan = start + 1; toScan < end; toScan++) {
+    for (int toScan = start + 1; toScan <= end; toScan++) {
       if (data[toScan] < pivot) {
         System.out.println("BEFORE: " + toString(data));
         int storage = data[bigInd];
         data[bigInd] = data[toScan];
         data[toScan] = storage;
-//        System.out.println("BigInd Before: " + bigInd);
+        System.out.println("BigInd Before: " + bigInd);
         bigInd++;
-//        System.out.println("BigInd After: " + bigInd);
+        System.out.println("BigInd After: " + bigInd);
 //        System.out.println("Actual Smaller Case: " + toString(data) + "\n -------------");
       }
       else if (data[toScan] == pivot) {
@@ -32,22 +33,24 @@ public class Preliminary {
         int bool = b.nextInt(2);
         if (bool == 0) {
           bigSmallSplit = false;
-//          System.out.println("System chose smaller!");
+          System.out.println("System chose smaller!");
           int toStore = data[bigInd];
           data[bigInd] = data[toScan];
           data[toScan] = toStore;
-//          System.out.println("BigInd Before: " + bigInd);
+          System.out.println("BigInd Before: " + bigInd);
           bigInd++;
-//          System.out.println("BigInd After: " + bigInd);
+          System.out.println("BigInd After: " + bigInd);
 //          System.out.println("\n -------------");
         }
+        System.out.println("System chose larger!");
       }
       System.out.println("AFTER: " + toString(data));
     }
+    for (int i = 1; i < bigInd; i++) {
+      data[i - 1] = data[i];
+    }
 
-    int temp = data[bigInd - 1];
-    data[bigInd - 1] = data[start];
-    data[start] = temp;
+    data[bigInd - 1] = pivot;
     System.out.println("DEBUG: Moved back into pivot spot" + toString(data));
 //    System.out.println("BigInd: " + bigInd);
 //    System.out.println("PivotValue: " + data[bigInd - 1]);
@@ -60,38 +63,20 @@ public class Preliminary {
   }
 
   private static int quickSelect(int[] data, int k, int start, int end) {
-  /*  if (data.length == 2) {
-      int first = data[0];
-      int second = data[1];
-      if (k == 0) {
-        if (first > second) {
-          return second;
-        }
-        else {
-          return first;
-        }
-      }
-      if (k == 1) {
-        if (first > second) {
-          return first;
-        }
-        else {
-          return second;
-        }
-      }
-    }
-    */
+    System.out.println("Start --> " + start + " end --> " + end);
     int resultInd = partition(data, start, end);
     if (resultInd == k) {
       return data[k];
     }
     if (resultInd < k) {
-      System.out.println(toString(data));
-      return quickSelect(data, k, resultInd + 1, end);
+      System.out.println("After Initial Partition " + toString(data));
+      System.out.println("Returned Index: " + resultInd);
+      return quickSelect(data, k, resultInd, end);
     }
     else {
-      System.out.println(toString(data));
-      return quickSelect(data, k, start, resultInd - 1);
+      System.out.println("After Initial Partition " + toString(data));
+      System.out.println("Returned Index: " + resultInd);
+      return quickSelect(data, k, start, resultInd);
     }
   }
 
