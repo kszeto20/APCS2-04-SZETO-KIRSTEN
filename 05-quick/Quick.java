19d4297aa67ee7b6d6915016f304a2885e659a10
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-15 04:47:26 -0400

    for development history, please look at the Preliminary.java file. I accidentally developed all the methods initially there. I only did some minor debugging for quickselection under Quick.java

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -3,19 +3,16 @@ import java.util.*;
 public class Quick {
   private static Random r = new Random();
 
-  public static int partition (int [] data, int start, int end){
-//    System.out.println(toString(data));
-    //Random r = new Random();
+  public static int partition (int [] data, int start, int end) {
+
     int index = r.nextInt(end - start + 1) + start;
-//    System.out.println("RandomIndex: " + index);
     int pivot = data[index];
-//    System.out.println("PivotValue: " + pivot);
+
     data[index] = data[start];
     data[start] = pivot;
 
     int storage = 0;
     int fiftyChance = 0;
-//    System.out.println("Initial move of P to the front: " + toString(data));
     int bigInd = start + 1;
 
     for (int toScan = start + 1; toScan <= end; toScan++) {
@@ -34,11 +31,6 @@ public class Quick {
     storage = data[bigInd - 1];
     data[bigInd - 1] = data[start];
     data[start] = storage;
-
-//    System.out.println("DEBUG: Moved back into pivot spot" + toString(data));
-//    System.out.println("BigInd: " + bigInd);
-//    System.out.println("PivotValue: " + data[bigInd - 1]);
-//   System.out.println("Kirsten's Pivot Index: " + (bigInd - 1));
     return (bigInd - 1);
   }
 
@@ -47,22 +39,15 @@ public class Quick {
   }
 
   private static int quickSelect(int[] data, int k, int start, int end) {
-//    System.out.println("Start --> " + start + " end --> " + end);
     int resultInd = partition(data, start, end);
 
     if (resultInd == k) {
-//      System.out.println("0.After Initial Partition " + toString(data));
-//      System.out.println("0.Returned Index: " + resultInd);
       return data[k];
-    } 
+    }
     if (resultInd < k) {
-//      System.out.println("1.After Initial Partition " + toString(data));
-//      System.out.println("1.Returned Index: " + resultInd);
       return quickSelect(data, k, resultInd + 1, end);
     }
     else {
-//      System.out.println("2.After Initial Partition " + toString(data));
-//      System.out.println("2.Returned Index: " + resultInd);
       return quickSelect(data, k, start, resultInd - 1);
     }
   }
