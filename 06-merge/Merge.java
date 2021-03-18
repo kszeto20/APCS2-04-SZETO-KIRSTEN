9681d8a912547ad37f410ced0ece620e95f9ea21
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-18 03:21:06 -0400

    changed it so that I'm just adding to a temp array not a left and right temp array for quicker outputs also stack ovevflow fixed; code now works somewhat

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -2,59 +2,49 @@ public class Merge {
 
   public static void mergeSort(int data[]) {
 
-    //// girl we gotta fix this
+    //// girl wtf we gotta fix this
     actualSort(data, 0, data.length - 1);
   }
 
   private static void mergeThat (int[] data, int lo, int middle, int hi) {
+    int[] temp = new int[hi - lo + 1];
+    // merge both sides
 
-    // right and left temp arrays to be made
+    int leftInitial = lo;
+    int rightInitial = middle + 1;
 
-    int left = (middle - lo) + 1;
-    int right = (hi - middle);
+    int totalInitial = 0;
 
-    int[] L = new int[left];
-    int[] R = new int[right];
-
-    // put in both sides into temp arrays
-
-    for (int i = 0; i < left; ++i) {
-      L[i] = data[lo + i];
+    while (leftInitial <= rightInitial && rightInitial <= hi) {
+      if (data[leftInitial] <= data[rightInitial]) {
+        temp[totalInitial] = data[leftInitial];
+        leftInitial++;
+        totalInitial++;
+      }
+      else {
+        temp[totalInitial] = data[rightInitial];
+        rightInitial++;
+        totalInitial++;
+      }
     }
-    for (int j = 0; j < right; ++j) {
-      R[j] = data[middle + 1 + j];
+
+
+    for (int h = lo; h < hi; h++) {
+      data[h] = temp[h - lo];
     }
-    System.out.println("Left: " + toString(L));
-    System.out.println("Right: " + toString(R));
-
-    System.out.println(toString(L) + toString(R));
-
   }
-
-
 
   private static void actualSort (int[] data, int lo, int hi) {
     // middle is inclusive
 
     if (lo < hi) {
-      int middle = ((hi - 1) / 2) + lo;
-      System.out.println("left || upper " + middle + " val at mid: " + data[middle]);
+      int middle = (hi + lo) / 2;
+      System.out.println("left || upper " + (middle) + " val at mid: " + data[middle]);
       actualSort(data, lo, middle);
-      System.out.println("right || lower " + (middle + 1) + " val at mid: " + data[middle + 1]);
+      System.out.println("right || lower " + (middle) + " val at mid: " + data[middle]);
       actualSort(data, middle + 1, hi);
       mergeThat(data, lo, middle, hi);
     }
-  }
-
-  public static String toString(int[] data) {
-    String toRet = "{";
-    for (int i = 0; i < data.length - 1; i++) {
-        toRet += data[i];
-        toRet += ", ";
-      }
-    toRet += data[data.length - 1];
-    toRet += "}";
-    return toRet;
   }
 }
 
