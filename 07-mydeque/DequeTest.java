63fd7d946fc13a5bd3824adb487ec0e4a88fd161
Kirsten Szeto <kszeto20@stuy.edu>
2021-03-21 23:07:22 -0400

    compiles; testing preliminary cases with current methods

---
+++ 07-mydeque/DequeTest.java
@@ -0,0 +1,38 @@
+public class DequeTest {
+  public static void main(String[] args) {
+    MyDeque test = new MyDeque();
+    System.out.println(test.size());
+    System.out.println(test.get(0));
+    System.out.println(test.get(1));
+    System.out.println(test.get(2));
+    System.out.println(test.get(3));
+    System.out.println(test.get(4));
+    System.out.println(test.get(5));
+    System.out.println(test.get(6));
+    System.out.println(test.get(7));
+    System.out.println("--------------------");
+    System.out.println(test.getStart());
+    System.out.println(test.getEnd());
+    System.out.println(test.get(0));
+    System.out.println(test.get(1));
+    System.out.println(test.get(2));
+    System.out.println(test.get(3));
+    System.out.println(test.get(4));
+    System.out.println(test.get(5));
+    System.out.println(test.get(6));
+    System.out.println(test.get(7));
+    System.out.println(test.getStart());
+    System.out.println(test.getEnd());
+    System.out.println(test.get(0));
+    System.out.println(test.get(1));
+    System.out.println(test.get(2));
+    System.out.println(test.get(3));
+    System.out.println(test.get(4));
+    System.out.println(test.get(5));
+    System.out.println(test.get(6));
+    System.out.println(test.get(7));
+    System.out.println(test.getStart());
+    System.out.println(test.getEnd());
+
+  }
+}
--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -5,18 +5,107 @@ public class MyDeque<E> {
   private int end;
 
   public MyDeque() {
-    data = (E[])new Object[10];
+    @SuppressWarnings("unchecked")
+    E[] d = (E[]) new Object[10];
+    data = d;
     size = 10;
     start = 0;
-    end = 9;
+    end = 0;
   }
 
   public MyDeque (int initialCapacity) {
-    data = (E[])new Object[initialCapacity];
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
     size = initialCapacity - 1;
     start = 0;
-    end = initialCapacity - 1;
+    end = 0;
   }
 
+  public int size() {
+    return size;
+  }
+  public E get( int ind) {
+    return data[ind];
+  }
+  public int getStart() {
+    return start;
+  }
+  public int getEnd() {
+    return end;
+  }
+/*
+  public int toString() {
+    int obLeft = size;
+    String toRet = "{";
+    for (int i = start; i <= data.length; i++) {
+      if (data[i] == null) {
+        toRet += ", ";
+      }
+      if (obLeft > 0 && i == data.length) {
+        i = 0;
+        toRet = toRet + data[i] + ", ";
+        obLeft--;
+      }
+      else if (obLeft > 0){
+        toRet = toRet + data[i] + ", ";
+        obLeft--;
+      }
+    }
+    toRet += "}";
+    return toRet;
+  }
+*/
+/*
+  public String toString() {
+    String full = "";
+    for (int i = 0; i < data.length; i++) {
+      if (data[i] == null) {
+        full += "";
+      }
+      else if(i == size - 1) {
+        full = full + data[i];
+      }
+      else {
+        full = full + data[i] + ", ";
+      }
+    }
+    return "[" + full + "]";
+  }
 
+  public void addFirst (E element) {
+    if (start - 1 < 0) {
+      data[data.length - 1] = element;
+      start = data.length - 1;
+    }
+    else {
+      data[start - 1] = element;
+      start = start--;
+    }
+  }
+
+  public void addLast (E element) {
+    if (end + 1 > data.length - 1) {
+      data[0] = element;
+      end = 0;
+    }
+    else {
+      data[end + 1] = element;
+      end++;
+    }
+  }
+
+  public E removeFirst() {
+    E toRet;
+    toRet += data[start];
+    start = start++;
+    return toRet;
+  }
+
+  public E removeLast() {
+    E toRet;
+    toRet += data[end];
+    end--;
+  }
+*/
 }
