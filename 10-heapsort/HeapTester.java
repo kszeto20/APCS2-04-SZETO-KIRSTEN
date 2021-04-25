b8846f6fd269ad38b0e9d46e5e54600c4ee3cb8a
Kirsten Szeto <kszeto20@stuy.edu>
2021-04-24 20:02:41 -0400

    works

--- 10-heapsort/HeapTester.java
+++ 10-heapsort/HeapTester.java
@@ -2,9 +2,16 @@ import java.util.*;
 import java.lang.*;
 public class HeapTester {
   public static void main (String[] args) {
-    int[] test = {90, 34, 99, 14, 89};
-    System.out.println("Before: " + Arrays.toString(test));
-    MyHeap.pushDown(test, 5, 0);
-    System.out.println("After: " + Arrays.toString(test));
+    int testSize = 5000000;
+    int[] test = new int[testSize];
+    Random r = new Random();
+    for (int i = testSize - 1; i >= 0; i--) {
+      test[i] = r.nextInt();
+    }
+
+    System.out.println("Before: " + MyHeap.toString(test));
+    MyHeap.heapsort(test);
+    System.out.println("After: " + MyHeap.toString(test));
+
   }
 }
--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,6 +1,44 @@
 import java.util.*;
 import java.lang.*;
 public class MyHeap {
+  public static void main (String[] args) {
+    int testSize = 500;
+    int[] test = new int[testSize];
+    int[] copy = new int[testSize];
+    Random r = new Random();
+    int toAdd;
+    for (int i = testSize - 1; i >= 0; i--) {
+      toAdd = r.nextInt(101);
+      test[i] = toAdd;
+      copy[i] = toAdd;
+    }
+    System.out.println("Before: " + Arrays.toString(test));
+
+    long startTime = System.currentTimeMillis();
+    Arrays.sort(copy);
+    long endTime = System.currentTimeMillis();
+    long timeElapsed = endTime - startTime;
+    System.out.println("Arrays.sort() Execution time in milliseconds: " + timeElapsed);
+    System.out.println("Array: " + Arrays.toString(copy));
+    startTime = System.currentTimeMillis();
+    heapsort(test);
+    endTime = System.currentTimeMillis();
+    timeElapsed = endTime - startTime;
+    System.out.println("My.sort() Execution time in milliseconds: " + timeElapsed);
+
+    System.out.println("After: " + Arrays.toString(test));
+
+    boolean match = true;
+    for (int i = 0; i < testSize; i++) {
+      if (test[i] != copy[i]) {
+        match = false;
+      }
+      if (!match) {
+        System.out.println("failed");
+      }
+    }
+    System.out.println(match);
+  }
   /*Swap the element at the provided index downward into the correct position.
   This will swap with the larger of the child nodes provided thatchild is larger.
   This stops when a leaf is reached, or neither child is larger.
@@ -10,8 +48,8 @@ public class MyHeap {
  *@precondition index is between 0 and size-1 inclusive
  *@precondition size is between 0 and data.length inclusive.
  */
-  public static void pushDown(int[]data, int size, int index) {
-    System.out.println("Array: " + Arrays.toString(data) + " Size: " + size + " index: " + index);
+  private static void pushDown(int[]data, int size, int index) {
+//    System.out.println("PDOWN ||||| Array: " + Arrays.toString(data) + " Size: " + size + " index: " + index);
     int left = 2 * index + 1;
     int right = 2 * index + 2;
     int change = index;
@@ -19,14 +57,14 @@ public class MyHeap {
     if (left < size) {
      if (data[left] > data[change]) {
        change = left;
-       System.out.println("left changed");
+//       System.out.println("left changed");
      }
     }
 
    if (right < size) {
      if (data[right] > data[change]) {
        change = right;
-       System.out.println("right changed");
+//       System.out.println("right changed");
      }
     }
 
@@ -34,8 +72,8 @@ public class MyHeap {
      int toChange = data[index];
      data[index] = data[change];
      data[change] = toChange;
-     System.out.println("Changed Arr: " + Arrays.toString(data));
-     System.out.println("Feeding in: \n" + Arrays.toString(data) + " " + size + " " + change);
+     //System.out.println("Changed Arr: " + Arrays.toString(data));
+//     System.out.println("PDOWN |||||| Feeding in: \n" + Arrays.toString(data) + " Size: " + size);
      pushDown(data, size, change);
    }
  }
@@ -43,19 +81,44 @@ public class MyHeap {
   /*Reorder the provided array to be a valid heap.
   *@param data is the array to be modified
   */
- //public static void buildHeap(int[]data)//We will discuss this today:
+  public static void buildHeap(int[]data){
+//    System.out.println("DATA LENGTH: " + data.length);
 //Before moving onto the next two methods, you must thoughroughly test the pushDown() and buildHeap() commands!
-
+    for (int i = data.length / 2 - 1; i >= 0; i--) {
+//      System.out.println("BHEAP |||||| Pushing Index: " + i + " || Value pushed: " + data[i]);
+      pushDown(data, data.length, i);
+    }
+  }
 
  /*Swap the root node with the element at the provided index.
   *Then push the new root down, but not past the index that it swapped with.
   *@precondition: size is between 0 and data.length-1 inclusive.
   */
- //private static void remove(int[]data,int size)
+ private static void remove(int[]data,int size) {
+   for (int i = size - 1; i >= 0; i--) {
+     //System.out.println("i val: " + i);
+     int toChange = data[0];
+     data[0] = data[i];
+     data[i] = toChange;
+//     System.out.println("Valid: ");
+/*
+     for (int j = 0; j < size; j++) {
+      System.out.println(data[j] + " ");
+     }
+*/
+     size--;
+     pushDown(data, size, 0);
+   }
+ }
 
  /*Sort the provided array
   *@param data is the array to be sorted
   */
- //public static void heapsort(int[]data)//We will discuss this next time!:
+ private static void heapsort(int[]data) {
+//   System.out.println("Building the heap");
+   buildHeap(data);
+//   System.out.println("Removing --------------------");
+   remove(data, data.length);
+ }
 
 }
